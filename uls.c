b083d22a0ddd439ddcc671cedffb2469e4fcee92
Eremin E. V <e.vl.eremin@yandex.ru>
2018-09-12 22:52:28 +0300

    uls: fix a relative path + LONG flag case

--- uls.c
+++ uls.c
@@ -7,25 +7,40 @@
 #include <sys/stat.h>
 #include <getopt.h>
 
+#define PAGE_SIZE 4096
+
 enum ls_flags {
 	LS_LONG = 0x1 << 0,
 };
+
+static inline size_t l_offset(void *p1, void *p2)
+{
+	return p2 - p1;
+}
 
 int do_ls(const char *path, unsigned flags)
 {
 	int r;
 	struct dirent *e = 0;
-	struct stat *st = malloc(sizeof(*st));
-	if (st) {
+	void *b = malloc(PAGE_SIZE);
+	if (b) {
+		struct stat *st = b;
+		int round_up = 0;	/* FIXME round up to at least 16b */
+		char *full = (void *) st + sizeof(*st) + round_up;
 		DIR *d = opendir(path);
 		if (d) {
 			errno = 0;
 			while ((e = readdir(d))) {
 				if (flags & LS_LONG) {
-					r = stat(e->d_name, st);
+					/*
+					 * compose full path for a relative
+					 * directory name (not a '.')
+					 */
+					snprintf(full, PAGE_SIZE - l_offset(b, full),
+						"%s/%s", path, e->d_name);
+					r = stat(full, st);
 					if (-1 == r) {
-						printf("uls: %s: %s\n",
-							e->d_name,
+						printf("uls: %s: %s\n", full,
 							strerror(errno));
 						errno = 0;
 						break;
@@ -43,7 +58,7 @@ int do_ls(const char *path, unsigned flags)
 		} else {
 			printf("uls: %s: %s\n", path, strerror(errno));
 		}
-		free(st);
+		free(b);
 	} else {
 		printf("uls: %s: %s\n", path, strerror(errno));
 	}
@@ -54,7 +69,8 @@ int main(int argc, char **argv, char **envp)
 	struct option o[] = {
 		{ "long", no_argument, 0, 'l' }
 	};
-	int c, i, flags = 0;
+	int c, i, nr = 0;
+	unsigned flags = 0;
 
 	while (-1 != (c = getopt_long(argc, argv, "l", o, &i))) {
 		switch (c) {
@@ -70,9 +86,12 @@ int main(int argc, char **argv, char **envp)
 	while (optind < argc) {
 		do_ls(*(argv + optind), flags);
 		optind++;
+		nr++;
 	}
 
-	do_ls(".", flags);
+	if (!nr) {
+		do_ls(".", flags);
+	}
 
 	return 0;
 }
